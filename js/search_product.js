57fdb067329327d2b90b6e831c0e160c9f513f50
Maksimyska <max.swimEKB@bk.ru>
2024-04-26 03:45:05 +0500

    Фикс продуктов

--- js/search_product.js
+++ js/search_product.js
@@ -1,4 +1,36 @@
-import productsData from './products.js';
+// import productsData from './products.js';
+const productsData = [
+    {
+        imgSrc: "/img/50 kop.png",
+        name: "Уникальные 50 коп.",
+        price: "500 р."
+    },
+    {
+        imgSrc: "/img/flowers.png",
+        name: "Горшок с цветами",
+        price: "743 р."
+    },
+    {
+        imgSrc: "/img/teapot.png",
+        name: "Электрический чайник",
+        price: "1000 р."
+    },
+    {
+        imgSrc: "/img/trash.png",
+        name: "Обычный стикер мусорки",
+        price: "463 р."
+    },
+    {
+        imgSrc: "/img/sword-CSS.png",
+        name: "Детский игрушечный меч",
+        price: "800 р."
+    },
+    {
+        imgSrc: "/img/car.png",
+        name: "Картинка машины",
+        price: "268 р."
+    }
+];
 
 // Функция для отображения информации о товаре на странице товара
 function displayProduct(product) {
